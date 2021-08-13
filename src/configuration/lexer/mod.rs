71bd73e124fb8fff50dd2d940e1397dabe9c31a9
hugues <ghugues@netc.fr>
2021-08-13 20:36:45 +0200

    Create specific mod for configuration::lexer

---
+++ src/configuration/lexer/mod.rs
@@ -0,0 +1,28 @@
+mod simple;
+
+#[derive(Debug)]
+pub enum Word {
+    Token(String),
+    Url(String),
+    Colon,
+    DefinitionSign,
+    Coma,
+
+    DirectoryCompositionBegin,
+    DirectoryCompositionEnd,
+    DirectoryConcatenateBegin,
+    DirectoryConcatenateEnd,
+
+    PipeDirectory2Directory,
+    PipeDirectory2File,
+    PipeFile2Directory,
+    PipeFile2File,
+
+    // for format
+    WhiteSpace(String),
+}
+
+// mettre numéro de ligne et de caractère
+pub fn lexer(_src: &str) -> impl Iterator<Item = Word> {
+    (0..1).map(|_| Word::DirectoryCompositionBegin)
+}
